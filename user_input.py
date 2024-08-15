a84f67a7a6bb5ad6aa8eaa8b6efba5503b8c18b5
Jado Fils <jasezikeye50@gmail.com>
2024-08-15 14:03:43 +0300

    The first python commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+fname=input('What your First name?\n');
+lname=input('What is your Lastname?\n')
+age=input('How old are you, please? ')
+location=input('Where are you now?')
+print(f'Hello?, My name is {fname, lname}, I come from {location} and I am {age} Years Old. Thank you')
